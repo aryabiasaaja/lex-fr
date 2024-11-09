f634565909e55805bb6a0c20ea57c4b94101de54
AryaJulianda <official.aryajulianda@gmail.com>
2024-11-10 01:24:05 +0700

    add force url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class PracticeArea
@@ -31,6 +32,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        };
+
         $practice_areas = [
             new PracticeArea(
                 'Employment',
